bf0b7a2dcd71c8799867e4b90b0190318c6cbfaa
Brian Murphy <90574883+bmtechart@users.noreply.github.com>
2024-02-21 18:40:14 -0700

    updated autoclave controller to store dials and door
    -added autoclave prefab
    -updated autoclave controller
    -added hinge joint to autoclave door.

--- Assets/_Core/Scripts/AutoclaveController.cs
+++ Assets/_Core/Scripts/AutoclaveController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VRTemplate;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -13,15 +14,24 @@ public class AutoclaveController : MonoBehaviour
 {
 
     [Header("Door")]
+    public GameObject Door;
+
     public UnityEvent OnDoorOpen;
     public UnityEvent OnDoorClosed;
     private DoorState _doorState;
+
+    [Header("Temperature")]
+    public XRKnob TemperatureDial;
+
+    [Header("Timer")]
+    public XRKnob TimerDial;
     public DoorState DoorState {  get { return _doorState; } }
     // Start is called before the first frame update
     void Start()
     {
         _doorState = DoorState.CLOSED;
     }
+
 
     // Update is called once per frame
     void Update()
